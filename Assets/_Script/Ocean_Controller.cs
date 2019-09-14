500d5476410830bc222c74a02b49f4adbc3c2241
SarmadSiddiqi <gigamine1234@gmail.com>
2019-09-14 12:34:10 -0400

    Scrolling
    Added Scrolling to the background

---
+++ Assets/_Script/Ocean_Controller.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ocean_Controller : MonoBehaviour
+{
+
+    public float verticalSpeed = 0.1f;
+    public bool resetToBottomPosition = false;
+
+    private float m_topResetPosition = 4.8f;
+    private float m_bottomResetPosition = -9.6f;
+    private bool m_isFirstRun = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Reset();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        CheckBounds();
+    }
+
+
+    void Reset()
+    {
+        Vector2 resetPosition = new Vector2(0.0f, 0.0f);
+        if (m_isFirstRun)
+        {
+            resetPosition.y = (resetToBottomPosition) ? m_bottomResetPosition : m_topResetPosition;
+            transform.position = new Vector2(0.0f, m_bottomResetPosition);
+            transform.position = resetPosition;
+            m_isFirstRun = false;
+            print("is first run");
+
+            m_topResetPosition = 9.6f;
+        }
+        else
+        {
+            transform.position = new Vector2(0.0f, m_topResetPosition);
+        }
+        
+    }
+
+    void CheckBounds()
+    {
+        if (transform.position.y <= -19.2f)
+        {
+            Reset();
+        }
+    }
+
+    void Move()
+    {
+        Vector2 currentPosition = transform.position;
+        currentPosition -= new Vector2(0.0f, verticalSpeed);
+        transform.position = currentPosition;
+    }
+}
